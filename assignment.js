fc2fc862ca3c02b65261e57298014313f0278919
unknown <you@example.com>
2026-01-14 21:52:07 +0600

    add task 5,6,7

--- assignment.js
+++ assignment.js
@@ -1,6 +1,8 @@
 // No-1
 // 1. Click করে color change করো:একটা button বানাও। button এ click করলে প্রথম li এর text color red হবে।
 
+// const { createElement } = require("react");
+
 // const fruitesList = document.querySelectorAll("li");
 
 // document.getElementById("btn").addEventListener("click", function () {
@@ -34,19 +36,11 @@
 // No-4
 // 4. নতুন fruit যোগ করো:button এ click করলে list এর শেষে Apple নামের নতুন li যোগ হবে।
 
-// me
-const listItem = document.getElementById("fruitesList");
-console.log(listItem);
+// const listItem = document.getElementById("fruitesList");
+// console.log(listItem);
 
-const li = document.createElement("li");
-console.log(li);
-
-li.innerText = "Orang";
-console.log(li);
-listItems.appendChild(li);
-
-// const listItems = document.getElementById("fruitesList");
-// console.log(listItems);
+// const btn = document.getElementById("btn");
+// console.log(btn);
 
 // const li = document.createElement("li");
 // console.log(li);
@@ -54,4 +48,46 @@ listItems.appendChild(li);
 // li.innerText = "Orang";
 // console.log(li);
 
-// listItems.appendChild(li);
+// btn.addEventListener("click", function () {
+//   const name = "siam";
+//   listItem.appendChild(li);
+// });
+
+// No-5
+// 5. Input থেকে value নিয়ে li বানাও:input এ যা লিখবে, button এ click করলে সেটা নতুন li হিসেবে list এ যোগ হবে।
+
+// const listItem = document.getElementById("fruitesList");
+// const btn = document.getElementById("btn");
+
+// btn.addEventListener("click", function () {
+//   const inputText = document.getElementById("inputText");
+//   const li = document.createElement("li");
+//   li.innerText = inputText.value;
+//   listItem.appendChild(li);
+
+//   inputText.value = "";
+// });
+
+// No-6
+// 6. Last li remove করো:button এ click করলে list এর শেষ li delete হবে।
+
+const listItem = document.getElementById("fruitesList");
+
+removeBtn.addEventListener("click", function () {
+  const lastLi = listItem.lastElementChild;
+
+  if (lastLi) {
+    listItem.removeChild(lastLi);
+  }
+});
+
+// No-7
+// 7. সব li এর color change করো:button এ click করলে সব li এর color blue হবে।
+
+colorBtn.addEventListener("click", function () {
+  const allLi = listItem.children;
+
+  for (let li of allLi) {
+    li.style.color = "blue";
+  }
+});
